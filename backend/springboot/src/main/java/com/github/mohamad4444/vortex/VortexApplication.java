081d42a0bb697a25a2119c66020382be766a89d6
Mohammad Mahamid <mzkariyah@gmail.com>
2024-06-14 01:09:36 +0200

    springboot initial

---
+++ backend/springboot/src/main/java/com/github/mohamad4444/vortex/VortexApplication.java
@@ -0,0 +1,13 @@
+package com.github.mohamad4444.vortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VortexApplication.class, args);
+	}
+
+}
---
+++ backend/springboot/src/test/java/com/github/mohamad4444/vortex/VortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mohamad4444.vortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
