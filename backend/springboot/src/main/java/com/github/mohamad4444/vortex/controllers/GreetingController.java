f92ee7bcc6471084b400cfb8176b9e659a5ddc4f
Mohammad Mahamid <mzkariyah@gmail.com>
2024-06-21 00:07:12 +0200

    Working Greeting example

--- backend/springboot/src/main/java/com/github/mohamad4444/vortex/controllers/GreetingController.java
+++ backend/springboot/src/main/java/com/github/mohamad4444/vortex/controllers/GreetingController.java
@@ -4,12 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	public String greeting(@RequestParam Map<String, String> params, Model model) {
+		model.addAttribute("fname", params.get("fname"));
+		model.addAttribute("lname", params.get("lname"));
 		return "greeting";
 	}
 
